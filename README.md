26911ef45019293d73dd2630d81439c622a9203d
2hnK <kimgh921@gmail.com>
2025-01-02 21:45:37 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Jump-To-SpringBoot3
+점프 투 스프링부트 (https://wikidocs.net/book/7601)
---
+++ src/main/java/com/mysite/sbb/user/UserService.java
@@ -0,0 +1,42 @@
+package com.mysite.sbb.user;
+
+import java.util.Optional;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.mysite.sbb.DataNotFoundException;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+
+	public SiteUser create(String username, String email, String password) {
+		SiteUser user = new SiteUser();
+		user.setUsername(username);
+		user.setEmail(email);
+		user.setPassword(passwordEncoder.encode(password));
+		this.userRepository.save(user);
+		return user;
+	}
+
+	public SiteUser getUser(String username) {
+		Optional<SiteUser> siteUser = this.userRepository.findByUsername(username);
+		if (siteUser.isPresent()) {
+			return siteUser.get();
+		} else {
+			throw new DataNotFoundException("siteuser not found");
+		}
+	}
+}
+
+/*
+ * @RequiredArgsConstructor: final 키워드가 붙은 필드를 초기화하는 생성자를 자동 생성하며,
+ * PasswordEncoder 타입의 빈을 컨테이너에서 가져와 주입합니다.
+ * 
+ * BCryptPasswordEncoder: 비크립트 해시 함수를 사용하는 암호화 기술
+ */
